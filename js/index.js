37582c99927e92fb5ddd5631c67d5409d3271392
lijanahmmed <lijanahmmed@gmail.com>
2025-08-28 00:46:38 +0600

    added copy button funtionalities

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 // Heart Icons Functionalities
 
 const hearts = document.getElementsByClassName("heart-icon");
-
 for (const heart of hearts) {
     heart.addEventListener("click", function () {
         const heartCounter = parseInt(document.getElementById("heart-count").innerText);
@@ -10,3 +9,17 @@ for (const heart of hearts) {
     })
 };
 
+
+// Copy Button Functionalities
+
+const copies = document.getElementsByClassName("copy-btn");
+for (const copy of copies) {
+    copy.addEventListener("click", function () {
+        const serviceNumber = copy.parentNode.parentNode.children[3].innerText;
+        navigator.clipboard.writeText(serviceNumber);
+        alert("Copied the service number" +" " + serviceNumber);
+        const copyCounter = parseInt(document.getElementById("copy-count").innerText);
+        const increaseCopyCount = copyCounter + 1;
+        document.getElementById("copy-count").innerText = increaseCopyCount;
+    })
+};
